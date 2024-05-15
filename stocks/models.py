5394ddd27ea3c87dda89320a650085a809c5bcf2
Jonathan Kjellen <jonathan.kjellen.3657@student.uu.se>
2024-05-15 21:11:13 +0200

    update model to use data format

--- stocks/models.py
+++ stocks/models.py
@@ -1,14 +1,19 @@
 from django.db import models
+import datetime
 
 # Create your models here.
 class Stock(models.Model):
     symbol = models.CharField(max_length=10)
-    name = models.CharField(max_length=100)
-    price = models.DecimalField(max_digits=10, decimal_places=2)
-    volume = models.IntegerField()
+    date = models.DateField(default=datetime.date.today)
+    open_price = models.DecimalField(max_digits=10, decimal_places=2)
+    high_price = models.DecimalField(max_digits=10, decimal_places=2)
+    low_price = models.DecimalField(max_digits=10, decimal_places=2)
+    close_price = models.DecimalField(max_digits=10, decimal_places=2)
+    volume = models.BigIntegerField()
+
+    def __str__(self):
+        return f"{self.symbol} - {self.date}"
+
     class Meta:
         app_label = 'stocks'
 
-
-    def __str__(self):
-        return self.symbol
